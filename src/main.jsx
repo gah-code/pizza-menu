a7a194879197c4e14aee0d5f1bc5054432d46cbd
Gilbert Haro <gilbertharocode@gmail.com>
2023-08-14 20:06:01 -0700

    end of section

--- src/main.jsx
+++ src/main.jsx
@@ -98,7 +98,7 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   return (
-    <li className='pizza'>
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h2>{pizzaObj.name}</h2>
